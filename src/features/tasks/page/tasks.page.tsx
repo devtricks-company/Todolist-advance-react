606a0866608b09a796f2be9e2c3f95800f0dac68
amir-mohammad <amirm.azarbashi@gmail.com>
2022-12-08 23:22:50 +0330

    first commit

---
+++ src/features/tasks/page/tasks.page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PageContainer } from "../../../shared/styles/page/page.styles";
+
+const TaskPage = () => {
+  return <PageContainer></PageContainer>;
+};
+
+export default TaskPage;
---
+++ src/shared/styles/page/page.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { PageType } from "../../types/page.type";
+
+export const PageContainer = styled.div<PageType>`
+  width: 100%;
+  display: ${(props) => props.display};
+  justify-content: ${(props) => props.justifyContent};
+  align-items: ${(props) => props.alignItems};
+  min-height: 100vh;
+`;
